923743a7c49dce05a54a1f976c63d5132188c3c5
CannaMuffinMan <cannamuffinman@gmail.com>
2025-11-25 18:35:29 -0500

    Add files via upload

---
+++ tailwind.config.ts
@@ -0,0 +1,29 @@
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      animation: {
+        'scanline': 'scanline 0.1s infinite',
+      },
+      keyframes: {
+        scanline: {
+          '0%': { transform: 'translateY(0)' },
+          '100%': { transform: 'translateY(100%)' },
+        }
+      }
+    },
+  },
+  plugins: [],
+}
+export default config

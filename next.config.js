aa84d0bf12023ac1e0b5f3ec130300a8cab59437
CannaMuffinMan <cannamuffinman@gmail.com>
2025-11-25 19:51:41 -0500

    Add output configuration to next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',           // ← THIS LINE MAKES THE "out" FOLDER
   reactStrictMode: true,
   swcMinify: true,
   images: {
     unoptimized: true
   }
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
